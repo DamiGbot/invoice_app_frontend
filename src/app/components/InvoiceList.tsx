77f1b6bad1ff3eb2c14a68c0cc89fa31108ee430
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2024-02-03 09:50:46 +0000

    feat: Adding the Empty Invoice list Page

--- src/app/components/InvoiceList.tsx
+++ src/app/components/InvoiceList.tsx
@@ -1,6 +1,7 @@
 import arrowDown from "../../../public/assets/icon-arrow-down.svg";
 import plus from "../../../public/assets/icon-plus.svg";
 import data from "../helpers/data.json";
+import illustrationEmpty from "../../../public/assets/illustration-empty.svg";
 
 import Image from "next/image";
 import Card from "./Card";
@@ -36,7 +37,7 @@ type Invoice = {
 
 export default function InvoiceList() {
 	// using this to test the no invoice state, remember to delete this
-	// data.length = 0;
+	data.length = 0;
 
 	return (
 		<>
@@ -48,7 +49,7 @@ export default function InvoiceList() {
 							Invoices
 						</p>
 						<p className="font-medium text-[12px] tracking-[-0.25px] text-[#888EB0]">
-							<span>{data.length == 0 ? "No" : data.length}</span> invoices
+							<span>{data.length === 0 ? "No" : data.length}</span> invoices
 						</p>
 					</div>
 
@@ -73,69 +74,89 @@ export default function InvoiceList() {
 			</section>
 
 			{/* Invoice list cards */}
-			<section className="mt-[32px] flex flex-col gap-[8px]">
-				{data.map((item: Invoice) => {
-					const capitalizedStatus =
-						item.status.charAt(0).toUpperCase() + item.status.slice(1);
+			<section
+				className={`section-container ${
+					data.length > 0 ? "mt-[32px] flex flex-col gap-[8px]" : ""
+				}`}
+			>
+				{/* I want to have conditional rendering based on the data length */}
 
-					return (
-						<Card key={item.id}>
-							{" "}
-							{/* Add a unique key prop */}
-							<div className="flex flex-col gap-[24px]">
-								<div className="flex justify-between">
-									<p className="font-bold text-[12px] tracking-[-0.25px] text-[#0C0E16] leading-[15px]">
-										<span className="text-[#7E88C3]">#</span>
-										{item.id}
-									</p>
-									<p className="font-medium text-[12px] tracking-[-0.25px] text-[#858BB2] leading-[15px]">
-										{item.clientName}
-									</p>
-								</div>
+				{data.length === 0 ? (
+					<div className="text-center h-full flex flex-1 flex-col justify-center items-center">
+						<Image src={illustrationEmpty} alt="there is no ivoice" />
+						<p className="mt-[40px] mb-[24px] font-bold text-[20px] tracking-[-0.63px] text-[#0C0E16]">
+							There is nothing here
+						</p>
+						<p className="font-medium text-[15px] tracking-[-0.25px] text-[#888EB0] leading-[15px]">
+							Create an invoice by cliking the <br />
+							<span className="font-bold">New</span> button and get started
+						</p>
+					</div>
+				) : (
+					data.map((item: Invoice) => {
+						const capitalizedStatus =
+							item.status.charAt(0).toUpperCase() + item.status.slice(1);
 
-								<div className="flex justify-between">
-									<div>
+						return (
+							<Card key={item.id}>
+								{" "}
+								{/* Add a unique key prop */}
+								<div className="flex flex-col gap-[24px]">
+									<div className="flex justify-between">
+										<p className="font-bold text-[12px] tracking-[-0.25px] text-[#0C0E16] leading-[15px]">
+											<span className="text-[#7E88C3]">#</span>
+											{item.id}
+										</p>
+										<p className="font-medium text-[12px] tracking-[-0.25px] text-[#858BB2] leading-[15px]">
+											{item.clientName}
+										</p>
+									</div>
+
+									<div className="flex justify-between">
 										<div>
-											<p className="font-medium text-[12px] tracking-[-0.25px] text-[#7E88C3] leading-[15px]">
-												<span className="text-[#888EB0]">Due</span> 19 Aug 2021
-											</p>
-											<p className="font-bold text-[16px] tracking-[-0.8px] text-[#0C0E16] leading-[24px]">
-												&#163; {item.total}{" "}
+											<div>
+												<p className="font-medium text-[12px] tracking-[-0.25px] text-[#7E88C3] leading-[15px]">
+													<span className="text-[#888EB0]">Due</span> 19 Aug
+													2021
+												</p>
+												<p className="font-bold text-[16px] tracking-[-0.8px] text-[#0C0E16] leading-[24px]">
+													&#163; {item.total}{" "}
+												</p>
+											</div>
+										</div>
+
+										<div
+											className={`w-[104px] h-[40px] flex justify-center items-center rounded-[6px] ${
+												capitalizedStatus === "Paid"
+													? "bg-[#33D69F] text-[#33D69F] bg-opacity-10"
+													: capitalizedStatus === "Pending"
+													? "bg-[#FF8F00] text-[#FF8F00] bg-opacity-10"
+													: capitalizedStatus === "Draft"
+													? "bg-[#373B53] text-[#373B53] bg-opacity-10"
+													: "" // Default style if status doesn't match
+											}`}
+										>
+											<div
+												className={`w-[8px] h-[8px] rounded-full mr-[8px] ${
+													capitalizedStatus === "Paid"
+														? "bg-[#33D69F] bg-opacity-100"
+														: capitalizedStatus === "Pending"
+														? "bg-[#FF8F00] bg-opacity-100"
+														: capitalizedStatus === "Draft"
+														? "bg-[#373B53] bg-opacity-100"
+														: "" // Default style if status doesn't match
+												}`}
+											></div>
+											<p className="font-bold text-[12px] tracking-[-0.25px] leading-[15px]">
+												{capitalizedStatus}
 											</p>
 										</div>
 									</div>
-
-									<div
-										className={`w-[104px] h-[40px] flex justify-center items-center rounded-[6px] ${
-											capitalizedStatus === "Paid"
-												? "bg-[#33D69F] text-[#33D69F] bg-opacity-10"
-												: capitalizedStatus === "Pending"
-												? "bg-[#FF8F00] text-[#FF8F00] bg-opacity-10"
-												: capitalizedStatus === "Draft"
-												? "bg-[#373B53] text-[#373B53] bg-opacity-10"
-												: "" // Default style if status doesn't match
-										}`}
-									>
-										<div
-											className={`w-[8px] h-[8px] rounded-full mr-[8px] ${
-												capitalizedStatus === "Paid"
-													? "bg-[#33D69F] bg-opacity-100"
-													: capitalizedStatus === "Pending"
-													? "bg-[#FF8F00] bg-opacity-100"
-													: capitalizedStatus === "Draft"
-													? "bg-[#373B53] bg-opacity-100"
-													: "" // Default style if status doesn't match
-											}`}
-										></div>
-										<p className="font-bold text-[12px] tracking-[-0.25px] leading-[15px]">
-											{capitalizedStatus}
-										</p>
-									</div>
 								</div>
-							</div>
-						</Card>
-					);
-				})}
+							</Card>
+						);
+					})
+				)}
 			</section>
 		</>
 	);
--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -3,5 +3,5 @@ type WrapperProps = {
 };
 
 export default function Wrapper({ children }: WrapperProps) {
-	return <main className="mx-6 my-8">{children}</main>;
+	return <main className="main mx-6 my-8">{children}</main>;
 }
